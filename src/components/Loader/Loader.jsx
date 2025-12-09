3bbf8c554400079316b3f1859bfb6c4715cda239
danbijyj3 <danbijyj3@gmail.com>
2025-12-09 22:52:41 +0900

    수정

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,6 +3,19 @@ import gsap from 'gsap';
 import './style.scss';
 
 const Loader = ({ onComplete }) => {
+    const repeatCount = 5;
+
+    const marquees = [
+        { className: 'clip_top', text: 'Hello World' },
+        { className: 'clip_center', text: 'JANGYOOJUNG' },
+        { className: 'clip_bottom', text: 'Portfolio' },
+    ];
+
+    const renderSpans = (text) =>
+        [...Array(repeatCount)].map((_, i) => (
+            <span key={i}>{text}&nbsp;</span>
+        ));
+
     useEffect(() => {
         const loader = document.querySelector('.loader');
         loader.classList.add('ready');
@@ -18,10 +31,10 @@ const Loader = ({ onComplete }) => {
         });
 
         const normalizeWidths = () => {
-            const marquees = gsap.utils.toArray('.marquee');
-            const widths = marquees.map((el) => el.scrollWidth);
+            const els = gsap.utils.toArray('.marquee');
+            const widths = els.map((el) => el.scrollWidth);
             const maxWidth = Math.max(...widths);
-            marquees.forEach((el) => {
+            els.forEach((el) => {
                 el.style.width = `${maxWidth}px`;
             });
         };
@@ -32,6 +45,7 @@ const Loader = ({ onComplete }) => {
 
             gsap.set('.clip_top', { height: 0 });
             gsap.set('.clip_bottom', { height: 0 });
+
             gsap.set('.clip_top .marquee', { x: '100%' });
             gsap.set('.clip_center .marquee', { x: '-100%' });
             gsap.set('.clip_bottom .marquee', { x: '100%' });
@@ -47,19 +61,11 @@ const Loader = ({ onComplete }) => {
         runAnimation();
 
         const handleResize = () => {
-            gsap.set(
-                [
-                    '.clip_top .marquee',
-                    '.clip_center .marquee',
-                    '.clip_bottom .marquee',
-                ],
-                { clearProps: 'transform' }
-            );
+            gsap.set('.marquee', { clearProps: 'transform' });
             runAnimation();
         };
 
         window.addEventListener('resize', handleResize);
-
         return () => {
             window.removeEventListener('resize', handleResize);
             tl.kill();
@@ -68,39 +74,15 @@ const Loader = ({ onComplete }) => {
 
     return (
         <div className="loader">
-            <section className="loader_clip clip_top">
-                <div className="marquee">
-                    <div className="marque_container">
-                        <span>Hello World&nbsp;</span>
-                        <span>Hello World&nbsp;</span>
-                        <span>Hello World&nbsp;</span>
-                        <span>Hello World&nbsp;</span>
-                        <span>Hello World&nbsp;</span>
+            {marquees.map((m, i) => (
+                <section key={i} className={`loader_clip ${m.className}`}>
+                    <div className="marquee">
+                        <div className="marquee_container">
+                            {renderSpans(m.text)}
+                        </div>
                     </div>
-                </div>
-            </section>
-            <section className="clip_center">
-                <div className="marquee">
-                    <div className="marquee_container">
-                        <span>JANGYOOJUNG&nbsp;</span>
-                        <span>JANGYOOJUNG&nbsp;</span>
-                        <span>JANGYOOJUNG&nbsp;</span>
-                        <span>JANGYOOJUNG&nbsp;</span>
-                        <span>JANGYOOJUNG&nbsp;</span>
-                    </div>
-                </div>
-            </section>
-            <section className="loader_clip clip_bottom">
-                <div className="marquee">
-                    <div className="marquee_container">
-                        <span>Portfolio&nbsp;</span>
-                        <span>Portfolio&nbsp;</span>
-                        <span>Portfolio&nbsp;</span>
-                        <span>Portfolio&nbsp;</span>
-                        <span>Portfolio&nbsp;</span>
-                    </div>
-                </div>
-            </section>
+                </section>
+            ))}
         </div>
     );
 };
