a54563e8411b2cd50d65f557b68ca05f9211f7f7
danbijyj3 <danbijyj3@gmail.com>
2025-12-11 19:30:47 +0900

    skills 수정

--- src/components/Sections/Skills/Skills.jsx
+++ src/components/Sections/Skills/Skills.jsx
@@ -51,7 +51,7 @@ const Skills = () => {
         ScrollTrigger.create({
             trigger: '.skills',
             start: 'top top',
-            end: '+=900',
+            end: '+=740',
             scrub: 0.5,
             pin: true,
             onUpdate: (self) => {
