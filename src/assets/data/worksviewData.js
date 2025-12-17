429cc7505322b092f7d9f7c3561d31124fae7dfa
danbijyj3 <danbijyj3@gmail.com>
2025-12-17 17:45:02 +0900

    worksview 수정

--- src/assets/data/worksviewData.js
+++ src/assets/data/worksviewData.js
@@ -1,43 +1,43 @@
 const items = [
     {
         img: '/images/wv_06.png',
-        parllaxSpeed: 0.065,
+        parallaxSpeed: 0.02,
     },
     {
         img: '/images/wv_09.png',
-        parllaxSpeed: 0.05,
+        parallaxSpeed: 0.05,
     },
     {
         img: '/images/wv_02.png',
-        parllaxSpeed: 0.08,
+        parallaxSpeed: 0.08,
     },
     {
         img: '/images/wv_04.png',
-        parllaxSpeed: 0.1,
+        parallaxSpeed: 0.12,
     },
     {
         img: '/images/wv_08.png',
-        parllaxSpeed: 0.07,
+        parallaxSpeed: 0.06,
     },
     {
         img: '/images/wv_03.png',
-        parllaxSpeed: 0.085,
+        parallaxSpeed: 0.15,
     },
     {
         img: '/images/wv_05.png',
-        parllaxSpeed: 0.06,
+        parallaxSpeed: 0.09,
     },
     {
         img: '/images/wv_01.png',
-        parllaxSpeed: 0.04,
+        parallaxSpeed: 0.03,
     },
     {
         img: '/images/wv_10.png',
-        parllaxSpeed: 0.1,
+        parallaxSpeed: 0.18,
     },
     {
         img: '/images/wv_07.png',
-        parllaxSpeed: 0.065,
+        parallaxSpeed: 0.11,
     },
 ];
 
